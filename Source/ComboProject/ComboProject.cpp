ba1dfb7f5b32049a553ba6add7dcf1a76b3830d2
Laurene <laurene.broquet@gmail.com>
2025-02-22 12:06:48 +0100

    Unreal 5.4.4 : third person template

---
+++ Source/ComboProject/ComboProject.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ComboProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ComboProject, "ComboProject" );
+ 
