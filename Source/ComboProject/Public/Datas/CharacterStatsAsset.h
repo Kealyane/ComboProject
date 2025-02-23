993ec35010b6e98c2a2d054cd6deaa064b3d0576
Laurene <laurene.broquet@gmail.com>
2025-02-23 15:47:02 +0100

    create character stats asset

---
+++ Source/ComboProject/Public/Datas/CharacterStatsAsset.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterStatsAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class COMBOPROJECT_API UCharacterStatsAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	float Health = 100.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	float Stamina = 50.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	float StaminaRegenRate = 2.f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	float BaseAttack = 5.f;
+};
