a497ef117b3175a9f559b672e0c74bcef2466808
Laurene <laurene.broquet@gmail.com>
2025-03-21 10:35:45 +0100

    add umg to build.cs

--- Source/ComboProject/ComboProject.Build.cs
+++ Source/ComboProject/ComboProject.Build.cs
@@ -8,6 +8,14 @@ public class ComboProject : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput", 
+			"UMG",
+		});
 	}
 }
