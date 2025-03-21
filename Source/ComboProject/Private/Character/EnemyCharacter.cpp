1a7f97b3924601bf792dbd17d3833b7ecfc62e36
Laurene <laurene.broquet@gmail.com>
2025-03-21 10:35:29 +0100

    create enemy character
    - change material instance color

---
+++ Source/ComboProject/Private/Character/EnemyCharacter.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/EnemyCharacter.h"
+
+#include "Character/CharacterStatsComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Widgets/EnemyWidget.h"
+
+// Sets default values
+AEnemyCharacter::AEnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	StatsComponent = CreateDefaultSubobject<UCharacterStatsComponent>(TEXT("StatsComponent"));
+	
+	// WidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("WidgetComponent"));
+	// WidgetComponent->SetupAttachment(RootComponent);
+	// WidgetComponent->SetVisibility(true);
+}
+
+// Called when the game starts or when spawned
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	EnemyWidget = Cast<UEnemyWidget>(WidgetComponent->GetWidget());
+	check(EnemyWidget);
+	check(StatsComponent);
+}
+
+void AEnemyCharacter::UpdateHealth(float AttackValue)
+{
+	StatsComponent->ChangeHP(-AttackValue);
+	EnemyWidget->SetHealthBar(StatsComponent->GetCurrentHP()/StatsComponent->GetMaxHP());
+}
+
+
+// Called to bind functionality to input
+void AEnemyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/ComboProject/Public/Character/EnemyCharacter.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnemyCharacter.generated.h"
+
+class UEnemyWidget;
+class UWidgetComponent;
+class UCharacterStatsComponent;
+class UCharacterStatsAsset;
+
+UCLASS()
+class COMBOPROJECT_API AEnemyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AEnemyCharacter();
+
+	UFUNCTION(BlueprintCallable)
+	UCharacterStatsComponent* GetCharacterStatsComponent() const { return StatsComponent; }
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCharacterStatsComponent> StatsComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UWidgetComponent> WidgetComponent;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	UFUNCTION()
+	void UpdateHealth(float AttackValue);
+
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetComponent(UWidgetComponent* InWidgetComponent) { WidgetComponent = InWidgetComponent; }
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	TObjectPtr<UEnemyWidget> EnemyWidget;
+
+};
